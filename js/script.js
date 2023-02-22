9f451c34c1cd6c63b33c79f27c4121a02a7e7353
danilopiccinni <danilopiccinni@gmail.com>
2023-02-22 20:05:48 +0100

    feat: calcolo biglietto / fine esercizio

--- js/script.js
+++ js/script.js
@@ -12,3 +12,64 @@
 -               : ALTRIMENTI 
 -                   ° comunichiamo all'utente il prezzo calcolato in partenza (quindi senza sconto)
 */
+
+let chilometri;
+let età;
+let prezzoKm = 0.21;
+let prezzoSenzaSconto;
+let prezzoScontato;
+
+
+chilometri = parseInt (prompt("Quanti chilometri desideri percorrere?"))
+
+età = parseInt (prompt("Indica la tua età perfavore"))
+
+prezzoSenzaSconto = chilometri * prezzoKm
+
+
+if (isNaN(chilometri) && isNaN(età)) {
+
+    document.writeln ("A quanto pare ho a che fare con un asino")
+    
+    } else 
+        if(isNaN(età)) {
+    
+            document.writeln ("Se non sai indicare correttamente quanti anni hai stai messo male fraté")
+    
+        } else 
+            if(isNaN(chilometri)) {
+    
+                document.writeln ("Se non sai quanto dista la tua destinazione puoi googlare e riprovare")
+    
+            } else 
+                if (età < 18) {
+    
+                    prezzoScontato = prezzoSenzaSconto = (prezzoSenzaSconto / 100 * 20)
+    
+                    prezzoScontato = prezzoScontato.toFixed(2)
+    
+                    console.log ("il prezzo del biglietto é " + prezzoScontato)
+    
+                    document.writeln("Il prezzo del viaggio é " + prezzoScontato + "€")
+    
+                } else
+                    if (età > 65) {
+    
+                        prezzoScontato = prezzoSenzaSconto = (prezzoSenzaSconto / 100 * 40)
+    
+                        prezzoScontato = prezzoScontato.toFixed(2)
+    
+                        console.log ("il prezzo del biglietto é " + prezzoScontato)
+    
+                        document.writeln("Il prezzo del viaggio é " + prezzoScontato + "€")
+    
+                    } else 
+                        {
+
+                        prezzoSenzaSconto = prezzoSenzaSconto.toFixed(2)
+
+                        console.log ("il prezzo del biglietto é " + prezzoSenzaSconto)
+
+                        document.writeln("Il prezzo del viaggio é " + prezzoSenzaSconto + "€")
+
+                        }
